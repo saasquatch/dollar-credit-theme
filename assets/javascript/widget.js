0b4419c4e4d247d6365a72edcd1bc13081515c6f
Jorge Conde <jorge@referralsaasquatch.com>
2016-11-01 12:43:52 -0700

    Add fb handler

--- assets/javascript/widget.js
+++ assets/javascript/widget.js
@@ -10,6 +10,8 @@
   }
 
   function handleClicks(elem, fn) {
+    if (!elem) return;
+
     if (document.addEventListener) {
       // For all major browsers, except IE 8 and earlier
       elem.addEventListener("click", fn, false);
@@ -106,29 +108,46 @@
     var scrollElements = document.querySelectorAll('[data-scroll-element]');
     var sendEmailBtn = document.getElementById('squatch-send-email');
     var emailInput = document.getElementById('squatch-user-email');
+    var facebookBtn = document.getElementsByClassName('fbShare')[0];
 
-    if (sendEmailBtn) {
-      handleClicks(sendEmailBtn, function() {
-        if (!isValidEmail(emailInput.value)) {
-          my_addClass(emailInput, 'invalid');
-          emailInput.onkeypress = function() {
-            if (isValidEmail(this.value)) {
-              my_removeClass(this, 'invalid');
-              my_addClass(this, 'valid');
-            }
-          }
-        } else {
-          my_removeClass(emailInput, 'invalid');
-          var registerForm = document.getElementsByClassName('squatch-register')[0];
-          registerForm.style.paddingTop = '30px';
-          registerForm.innerHTML = '<p><strong>' + emailInput.value + '</strong><br>Has been successfully registered</p>';
 
-          if (window.parent.squatch && window.parent.squatch.eventBus) {
-            window.parent.squatch.eventBus.dispatch('email_submitted', this, emailInput.value /*, JWT*/);
+    handleClicks(sendEmailBtn, function() {
+      if (!isValidEmail(emailInput.value)) {
+        my_addClass(emailInput, 'invalid');
+        emailInput.onkeypress = function() {
+          if (isValidEmail(this.value)) {
+            my_removeClass(this, 'invalid');
+            my_addClass(this, 'valid');
           }
         }
-      });
-    }
+      } else {
+        my_removeClass(emailInput, 'invalid');
+        var registerForm = document.getElementsByClassName('squatch-register')[0];
+        registerForm.style.paddingTop = '30px';
+        registerForm.innerHTML = '<p><strong>' + emailInput.value + '</strong><br>Has been successfully registered</p>';
+
+        if (window.parent.squatch && window.parent.squatch.eventBus) {
+          window.parent.squatch.eventBus.dispatch('email_submitted', this, emailInput.value /*, JWT*/);
+        }
+      }
+    });
+
+    handleClicks(facebookBtn, function(e) {
+      // If it's not mobile, don't use href link
+      if (e.type != 'touchstart') {
+        e.preventDefault();
+
+        var pictureString = (squatch.user.facebook.shareImage == "" || squatch.user.facebook.shareImage === null) ? "" : "&picture="+squatch.user.facebook.shareImage;
+
+        var url= "https://www.facebook.com/dialog/feed?app_id=" + squatch.user.facebook.appId + "&link=" + squatch.user.facebook.link + "&name=" + squatch.user.facebook.title + "&description=" + squatch.user.facebook.summary + pictureString+ "&redirect_uri=" + squatch.user.facebook.redirectUrl + "&display=popup";
+
+        window.open(url, 'fb', 'status=0,width=620,height=400');
+      }
+
+      if (window.parent.squatch && window.parent.squatch.eventBus) {
+        window.parent.squatch.eventBus.dispatch('fb_btn_clicked', this, /*params*/ /*, JWT*/);
+      }
+    });
 
     var inValidRange = function(offset, limit) {
       return offset >= 0 && offset < limit;
