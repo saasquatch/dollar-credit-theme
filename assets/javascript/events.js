72296b7f4aec2931293205cbdce7887bf71aa7d2
jorgecgll <jorge@referralsaasquatch.com>
2017-02-22 10:14:27 -0800

    JC - try new schema

--- assets/javascript/events.js
+++ assets/javascript/events.js
@@ -57,6 +57,7 @@ function smsHandler() {
 
   // Test url
   var smsUrl = 'sms:?&body=' + squatch.user.sms.body;
+  console.log('smsUrl', smsUrl);
   smsBtn.href = smsUrl;
 
   var md = new MobileDetect('Version/4.0 Mobile Safari/534.30');
